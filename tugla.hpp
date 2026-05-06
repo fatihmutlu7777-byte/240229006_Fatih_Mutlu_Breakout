bb18f3e1658b645a3ce8bfadb7835c478e2ab19f
fatihmutlu7777-byte <fatihmutlu7777@gmail.com>
2026-05-06 11:20:58 +0300

    Tuğla.cpp eklendi ve yapıcı ile yıkıcı fonksyonlar tanımlandı.

---
+++ tugla.hpp
@@ -0,0 +1,30 @@
+#ifndef TUGLA_HPP
+#define TUGLA_HPP
+#include <SFML/Graphics.hpp>
+
+class Tugla{
+
+    public:
+     
+    sf::RectangleShape tuglanin_sekli;
+    bool kirildi_mi=false;//Tuğlalar kırıkmı değilmi kontrol ederiz
+
+    sf::FloatRect getSinirlar() { return tuglanin_sekli.getGlobalBounds(); }
+
+
+    //Yapıcı fonksyonlar
+
+    Tugla(float x,float y,float genislik ,float yukseklik,sf::Color renk);
+    void ciz(sf::RenderWindow& pencere);
+
+    //Yıkıcı fonksyon(tuğla için)
+    ~Tugla() = default;
+
+
+    void ciz(sf::RenderWindow& pencere);
+
+};
+
+
+
+#endif
